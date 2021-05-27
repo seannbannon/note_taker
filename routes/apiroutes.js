3c8a9e9a50a73195faff3cf71c8f918d30684d11
Sean Bannon <seannbannon@gmail.com>
2021-05-27 13:16:51 -0500

    const notedate to let notedata

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,7 +2,7 @@
 // LOAD DATA: linking route(s) to a series of "data" sources.
 const fs = require("fs");
 const router = require("express").Router();
-const noteData = require("../db/db.json");
+let noteData = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid"); // UUID to create a unique ID
 
 // ROUTING (CRUD operations)
